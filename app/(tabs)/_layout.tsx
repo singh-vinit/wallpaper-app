06d0b010be7b0941dd4fd14a511a06d63c9e538f
Vinit <singhvinit435@gmail.com>
2024-11-02 22:33:50 +0530

    bottom navigation tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "for you",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "explore",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="explore" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "account",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user-circle" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function () {
-  return (
-    <View>
-      <Text>Hi there vinit</Text>
-    </View>
-  );
-}
